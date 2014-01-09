6afb8e768ce22a4b34b9a737434e622b5b5bc119
Harry Lawrence <harry.lawrence@groundsix.com>
2014-01-09 18:40:35 +0000

    Moved str_replace to seperate file

--- C/1/main.c
+++ C/1/main.c
@@ -1,46 +1,5 @@
 #include <stdio.h>
-#include <string.h>
-
-char *str_replace(char initial[], char replace[], char with[], char dest[])
-{
-    int i = 0;
-    int r = 0;
-
-    int index_count = 0;
-    int pos_indexes[100];
-
-    int initial_length = 0;
-
-    // TODO: Replace hardcoded 100
-    char fully_formed_string[100];
-
-    for (i = 0; i < strlen(initial);) {
-        if (initial[i] != replace[r]) {
-            r++;
-        } else {
-            fully_formed_string[i] = replace[i];
-            i++; r++;
-            
-            if (strcmp(fully_formed_string,replace) == 0) {
-
-                for (int rep = 0; rep < index_count; rep++) {
-                    initial[pos_indexes[rep]] = with[rep];
-                }
-
-                int final_string_count = 0;
-                while (strlen(initial) != strlen(dest)) {
-                    dest[final_string_count] = initial[final_string_count];
-                    final_string_count++;
-                }
-            }
-            pos_indexes[index_count] = i;
-            index_count++;
-        }
-    }
-    dest = initial;
-    initial = "";
-    return dest;
-}
+#include "str_replace.h"
 
 int main()
 {
---
+++ C/1/str_replace.h
@@ -0,0 +1,6 @@
+#ifndef STR_REPLACE_H
+#define STR_REPLACE_H
+
+char *str_replace(char initial[], char replace[], char with[], char dest[]);
+
+#endif
