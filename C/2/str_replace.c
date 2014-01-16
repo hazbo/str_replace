1624b17a962ea95f7c4414bfae37a33379390781
Harry Lawrence <harry.lawrence@groundsix.com>
2014-01-16 14:01:36 +0000

    Removed bin started structure for version 2

--- C/2/str_replace.c
+++ C/2/str_replace.c
@@ -1,14 +1,37 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h> // remove after debugging
 #include "str_replace.h"
 
 char *str_replace(char *orig, char *rep, char *with)
 {
-    int orig_count = strlen(orig);
-    int rep_count  = strlen(rep);
-    int with_count = strlen(with);
+    int len_orig;
+    int len_rep;
+    int len_with;
+    int count;
 
-    
+    if (!orig)
+        return NULL;
+
+    if (strstr(orig, rep) != NULL) {
+        if (!rep) {
+            rep = "";
+        }
+
+        if (!with) {
+            with = "";
+        }
+
+        len_rep  = strlen(rep);
+        len_with = strlen(with);
+        len_orig = strlen(orig);
+
+        for (int i = 0; i < len_orig; i++) {
+
+        }
+    } else {
+        return orig;
+    }
 
     return orig;
 }
